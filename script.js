116cf75577eba4793b4977a59f80fe879f96323b
Jake-Zaruba <94652629+Jake-Zaruba@users.noreply.github.com>
2022-05-18 01:46:45 -0500

    Added functionality
    Added functionality to the location and date. These parameters will now update depending on the user's location and the current month, day of the month, and day of the week.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 "use strict";
 
+//////////////////////////////////////////////////////////////
+//WEATHER API///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let currentCity = document.querySelector(`.city`);
 window.addEventListener(`load`, () => {
   let lon;
   let lat;
@@ -19,7 +23,9 @@ window.addEventListener(`load`, () => {
         .then((data) => {
           console.log(data);
           const { temp } = data.main;
+          const cityName = data.name;
           currentTemp.innerHTML = `&nbsp;` + Math.round(temp) + `&deg;`;
+          currentCity.innerHTML = cityName.toUpperCase();
         });
     });
   } else {
@@ -39,14 +45,121 @@ let sunGlow = document.getElementById(`sun-glow`);
 let sunset = document.getElementById(`sunset`);
 let sunsetGlow = document.getElementById(`sunset-glow`);
 let cloudGlow = document.getElementById(`cloud-glow`);
-let city = document.querySelector(`.city`);
+// let city = document.querySelector(`.city`);
 let displayDate = document.querySelectorAll(`.date`);
 let d = new Date();
 console.log(d);
 const time = d.getHours();
-// const time = 20;
-// const time = 10;
-console.log(time);
+// console.log(time);
+
+//////////////////////////////////////////////////////////////
+//DAY OF WEEK CHANGES///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let dayOfWeek = document.getElementById(`day`);
+let day = d.getDay();
+
+switch (day) {
+  case 0:
+    day = `SUNDAY`;
+    break;
+  case 1:
+    day = `MONDAY`;
+    break;
+  case 2:
+    day = `TUESDAY`;
+    break;
+  case 3:
+    day = `WEDNESDAY`;
+    break;
+  case 4:
+    day = `THURSDAY`;
+    break;
+  case 5:
+    day = `FRIDAY`;
+    break;
+  case 6:
+    day = `SATURDAY`;
+    break;
+}
+// console.log(day);
+
+dayOfWeek.innerHTML = day;
+
+//////////////////////////////////////////////////////////////
+//MONTH CHANGES/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let displayMonth = document.getElementById(`month`);
+let month = d.getMonth();
+
+switch (month) {
+  case 0:
+    month = `JANUARY`;
+    break;
+  case 1:
+    month = `FEBRUARY`;
+    break;
+  case 2:
+    month = `MARCH`;
+    break;
+  case 3:
+    month = `APRIL`;
+    break;
+  case 4:
+    month = `MAY`;
+    break;
+  case 5:
+    month = `JUNE`;
+    break;
+  case 6:
+    month = `JULY`;
+    break;
+  case 7:
+    month = `AUGUST`;
+    break;
+  case 8:
+    month = `SEPTEMBER`;
+    break;
+  case 9:
+    month = `OCTOBER`;
+    break;
+  case 10:
+    month = `NOVEMBER`;
+    break;
+  case 11:
+    month = `DECEMBER`;
+    break;
+}
+// console.log(month);
+
+displayMonth.innerHTML = month + `&nbsp;`;
+
+//////////////////////////////////////////////////////////////
+//MONTH CHANGES/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let displayDay = document.getElementById(`day-of-month`);
+let dayOfMonth = d.getDate();
+
+const getDayEnding = function (dayNumber) {
+  let dayEnding;
+  if (dayNumber === 1 || dayNumber === 21 || dayNumber === 31) {
+    dayEnding = `st`;
+  } else if (dayNumber === 2 || dayNumber === 22) {
+    dayEnding = `nd`;
+  } else if (dayNumber === 3 || dayNumber === 23) {
+    dayEnding = `rd`;
+  } else {
+    dayEnding = `th`;
+  }
+  // console.log(dayEnding);
+  return dayEnding;
+};
+// console.log(dayOfMonth);
+// console.log(getDayEnding(dayOfMonth));
+let dayEnding = getDayEnding(dayOfMonth);
+displayDay.innerHTML = dayOfMonth + `${dayEnding}`;
+
+//////////////////////////////////////////////////////////////
+//TIME OF DAY ANIMATION CHANGES/////////////////////////////////////////////////////////////////////////////////////////////
 
 if (time > 19 || time < 6) {
   weatherBackground.style.background = `linear-gradient(to bottom, #116687, rgba(13, 69, 102, 0))`;
@@ -59,11 +172,11 @@ if (time > 19 || time < 6) {
   moon.style.display = `block`;
   moonGlow.style.display = `block`;
   fog.style.display = `block`;
-  city.style.color = `white`;
+  currentCity.style.color = `white`;
   displayDate.forEach((entries) => {
     entries.style.color = `white`;
   });
-} else if (time === 6 || time === 18) {
+} else if (time === 6 || time === 19) {
   weatherBackground.style.background = `linear-gradient(to bottom, #f7e5a5, #fff8d2)`;
   sun.style.display = `none`;
   sunGlow.style.display = `none`;
